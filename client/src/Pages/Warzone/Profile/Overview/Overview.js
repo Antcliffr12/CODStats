9739b3bab773ef08df619cbb9d4ab1d7cc1c545a
Ryan Antcliff <34320424+Antcliffr12@users.noreply.github.com>
2021-08-24 12:07:42 -0400

    Add files via upload

--- client/src/Pages/Warzone/Profile/Overview/Overview.js
+++ client/src/Pages/Warzone/Profile/Overview/Overview.js
@@ -1,33 +1,64 @@
-import React, {useState, useEffect} from 'react'
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+
+import { useParams } from "react-router-dom";
 import axios from 'axios';
+import './Overview.scss';
 
+import Layout from '../../../../Components/Layout/Layout';
+import {Tabs, Tab} from "../../../../Components/Tabs/Tabs"
+import GetAllDataOverview from '../../../../Components/GetAllDataOverview/GetAllDataOverview';
+import Matches from '../../../../Components/Matches/Matches';
+import ProfileInfo from '../../../../Components/ProfileInfo/ProfileInfo';
 
-export default function Overview() {
-    const [wzData, setWzData] = useState(null);
-    const location = useLocation();
+function Overview({match}) {
+    //in order to get lenght state has be array 
+    // const [isLoading, setLoading] = useState(true);
+    // useEffect(() => {
+    //     fetchItem();
+    //  }, []);
 
-    useEffect(() => {
-       callApi(location.state.platform, location.state.username)
-    }, [location])
+    // const fetchItem = async () => {
+    //     const fetchItem = await fetch(`http://localhost:3001/api/warzone/standard/matches/${match.params.platform}/${match.params.username}`);
+    //     const item = await fetchItem.json();
+    //     setTimeout(function () {
+    //         setWzData(item.data);
+    //         setLoading(false);
 
-    const callApi = async (platform, username) => {
-       await axios.get(`http://localhost:3001/warzone/profile/${platform}/${username}/overview`)
-            .then(response => {
-                const wzData = response.data.data;
-                setWzData({ wzData })
-            })
-            .catch(function (error) {
-            console.log(error.response.data);
-        });
-    }
+    //     }, 500)
+    // }
+
+    // let display; 
+    // if (isLoading) {
+    //     display = <img className="imageCenter" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" />;
+    // } else {
+    //     display = <div className="container">
+    //             <Tabs>
+    //                 <Tab label={"overview"} tabName={"Overview"}>
+    //                     <Matches platform={match.params.platform} username={match.params.username} />
+    //                 </Tab>
+    //                 <Tab label={"subscribed"} tabName={"Subscribed"}>
+    //                     <h1>Subscribed</h1>
+    //                 </Tab>
+    //             </Tabs>
+    //         </div>
+    // }
 
     return (
-        <div>
-            <h1>Overview</h1>
-            <ul>
-                { console.log(wzData.wzData.title)}
-            </ul>
-        </div>
+        <Layout>
+            <div className="wz_cover" style={{backgroundImage: `url("https://trackercdn.com/cdn/cod.tracker.gg/warzone/images/warzone-profile.jpg")`}}>
+                <div className="wz_cover-overlay"></div>
+            </div>
+            <ProfileInfo user={match.params.username}/>
+            <Tabs>
+                    <Tab label={"overview"} tabName={"Overview"}>
+                        <Matches platform={match.params.platform} username={match.params.username} />
+                    </Tab>
+                    <Tab label={"subscribed"} tabName={"Subscribed"}>
+                        <h1>Subscribed</h1>
+                    </Tab>
+                </Tabs>
+        </Layout>
     )
 }
+
+export default Overview;
